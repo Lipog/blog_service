ba551759568987db35fc1c53216e4a13f05662cb
runoob <test@runoob.com>
2020-07-22 16:24:57 +0800

    完成了解析yaml的部分，通过viper进行解析，并与运行所需要的数据进行关联

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module blog-service
 go 1.12
 
 require github.com/gin-gonic/gin v1.6.3
+
+require github.com/spf13/viper v1.4.0
---
+++ pkg/setting/section.go
@@ -0,0 +1,41 @@
+package setting
+//用于声明配置属性的结构体，并编写读取区段配置的配置方法
+
+import "time"
+
+type ServerSettingS struct {
+	RunMode string
+	HttpPort string
+	ReadTimeout time.Duration
+	WriteTimeout time.Duration
+}
+
+type AppSettingS struct {
+	DefaultPageSize int
+	MaxPageSize int
+	LogSavePath string
+	LogFileName string
+	LogFileExt string
+}
+
+type DatabaseSettingS struct {
+	DBType string
+	UserName string
+	Password string
+	Host string
+	DBName string
+	TablePrefix string
+	Charset string
+	ParseTime bool
+	MaxIdleConns int
+	MaxOpenConns int
+}
+
+//读取配置信息，并将具体的配置信息写入到v中
+func (s *Setting) ReadSection(k string, v interface{}) error {
+	err := s.vp.UnmarshalKey(k, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
