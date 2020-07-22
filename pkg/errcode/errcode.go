1e7f917859e8c9415204c3d416a897c6066d5f90
runoob <test@runoob.com>
2020-07-22 15:48:44 +0800

    构建了Error结构体，并完成了对应的方法，用于统一处理错误

---
+++ pkg/errcode/errcode.go
@@ -0,0 +1,79 @@
+package errcode
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Error struct {
+	code int `json:"code"`
+	msg string `json:"msg"`
+	details []string `json:"details"`
+}
+
+var codes = map[int]string{}
+
+func NewError(code int, msg string) *Error {
+	//首先判断错误码是否已经存在于全局的map中
+	if _, ok := codes[code]; ok {
+		panic(fmt.Sprintf("错误码 %d 已经存在，请更换一个", code))
+	}
+
+	//不存在则新建，并放入到全局的map中去
+	codes[code] = msg
+	return &Error{
+		code:    code,
+		msg:     msg,
+	}
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("错误码: %d, 错误信息: %s", e.Code(), e.Msg())
+}
+
+func (e *Error) Code() int {
+	return e.code
+}
+
+func (e *Error) Msg() string {
+	return e.msg
+}
+
+func (e *Error) Msgf(args []interface{}) string {
+	return fmt.Sprintf(e.msg, args...)
+}
+
+func (e *Error) Details() []string {
+	return e.details
+}
+
+func (e *Error) WithDetails(details ...string) *Error {
+	e.details = []string{}
+	for _, d := range details {
+		e.details = append(e.details, d)
+	}
+	return e
+}
+
+func (e *Error) StatusCode() int {
+	switch e.Code() {
+	case Success.Code():
+		return http.StatusOK
+	case ServerError.Code():
+		return http.StatusInternalServerError
+	case InvalidParams.Code():
+		return http.StatusBadRequest
+	case UnauthorizedAuthNotExist.Code():
+		fallthrough
+	case UnauthorizedTokenError.Code():
+		fallthrough
+	case UnauthorizedTokenGenerate.Code():
+		fallthrough
+	case UnauthorizedTokenTimeout.Code():
+		return http.StatusUnauthorized
+	case TooManyRequests.Code():
+		return http.StatusTooManyRequests
+	}
+
+	return http.StatusInternalServerError
+}
